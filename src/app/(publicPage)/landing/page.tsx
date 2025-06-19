2d921064d46987bc6d53b22fd4c6635f4adfaf51
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-06-19 20:24:17 +0600

    landing page((banner section)

--- src/app/(publicPage)/landing/page.tsx
+++ src/app/(publicPage)/landing/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "./Banner";
+import FeatureSection from "./FeatureSection";
 
 export default function LandingPage() {
   return (
     <div>
       <Banner />
+      <FeatureSection />
     </div>
   );
 }
